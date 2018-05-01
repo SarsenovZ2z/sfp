2674f35d9592a2bbe8e00668ca79d0baaa089cb8
SarsenovZ2z <nurik9293709@gmail.com>
2018-05-01 16:18:45 +0600

    +flashlight +button

--- app/src/main/java/com/example/nurik/sfp/MainActivity.java
+++ app/src/main/java/com/example/nurik/sfp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.nurik.sfp;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void startEncoding(View v) {
+//        Camera camera = Camera.open();
+//        Camera.Parameters parameters = camera.getParameters();
+//
+//        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+//        camera.setParameters(parameters);
+//        camera.startPreview();
+
+//        EditText textMorse = (EditText)findViewById(R.id.textMorse);
+//        String data = textMorse.getText().toString();
+//        textMorse.setText(new StringBuilder(data).reverse().toString());
+//        textMorse.setText("");
+
+    }
+
+
 }
